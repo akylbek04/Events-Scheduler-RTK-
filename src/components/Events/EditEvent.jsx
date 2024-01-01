fb177b23c3c4a13437c5d5ee319a6cecd2132218
Akylbek04 <zamirovakylbek920@gmail.com>
2024-01-01 22:08:59 +0100

    used queryClient: {setQueryData, getQueryData, cancelQueries, invalidateQueries} along with onError, onSettled to implement OPTIMISTIC UPDATING

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,20 +1,81 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { id } = useParams();
 
-  function handleSubmit(formData) {}
+  const { data, isError, isPending, error } = useQuery({
+    queryKey: ["events", id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: id }),
+  });
 
-  function handleClose() {
-    navigate('../');
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ["events", id] });
+      const prevData = queryClient.getQueryData(["events", id]);
+
+      queryClient.setQueryData(["events", id], newEvent);
+
+      return { prevEvent: prevData };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", id], context.prevEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["events", id]);
+    },
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id: id, event: formData });
+    navigate("../");
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  function handleClose() {
+    navigate("../");
+  }
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div className="center">
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
+  if (isError)
+    content = (
+      <>
+        <ErrorBlock
+          title="Failed to fetch an event"
+          message={
+            error.info?.message ||
+            "Failed to load an event. Please check inputs and try again later."
+          }
+        />
+        <div className="form-actions">
+          <Link to="../" className="button">
+            Okay
+          </Link>
+        </div>
+      </>
+    );
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +83,12 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
-    </Modal>
-  );
+    );
+  }
+
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
+
+export const loader = () => {
+  queryClient.fetchQuery();
+};
